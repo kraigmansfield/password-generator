c39332e1e3bdbe616be9a75ff75060d98d4547da
kraigmansfield <Kraig.mansfield@gmail.com>
2023-01-08 13:42:51 -0900

    attempt 2 updates

--- assets/main.js
+++ assets/main.js
@@ -2,30 +2,97 @@
 var generateBtn = document.querySelector("#generate");
 
 
-//Arrays needed for password characters
-var charNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
-var charSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var passOriginalLength = 0;
+//Password criteria for generation/set initial characters to false
 
 
-//Confirmation declarations
-var confirmPassLength = "";
-var confirmSpecialCharacters;
-var confirmNumericCharacters;
-var confirmUpperCharacters;
-var confirmLowerCharacters;
+let password = {
+    passwordLength: 0,
+    upperCaseLetters: false,
+    lowerCaseLetters: false,
+    numbers: false,
+    specialCharacters: false,
+    lowerCasePool: 'abcdefghijklmnopqrstuvwxyz',
+    upperCasePool: 'ABSCEFGHIJKLMNOPQRSTUVWXYZ',
+    numberPool: '0123456789',
+    specialCharacterPool: '!@#$%^&*()',
+}
 
-//Global var for individual results of prompts
-var resultUC = "";
-var resultLC = "";
-var resultSC = "";
-var resultNum = "";
 
-//TODO: Promt #1: Asking a user how long they would like their password to be. If a password is outside parameters, return to question.
-//If condition
 //
+
+//Confirm users password length is between 8-128 characters
+
+function collectPasswordLength() {
+    var confirmPassLength = parseInt(prompt("Please select a password length between 8 and 128 characters!"));
+    if (!confirmPassLength) {
+        alert("Please enter a value!")
+    } else if (confirmPassLength < 8 || confirmPassLength > 128) {
+        confirmPassLength = parseInt(prompt("Please choose between 8 and 128!"))
+    } return
+    }
+
+// Used to determine what the user wants in their password pool.
+function validatePrompts() {
+    confirmUpperCharacters = window.confirm("Should it contain uppercase letters? (Ok = Yes | Cancel = No)");
+    confirmLowerCharacters = window.confirm("Should it contain lowercase letters? (Ok = Yes | Cancel = No)");
+    confirmNumericCharacters = window.confirm("Should it contain numbers? (Ok = Yes | Cancel = No)");
+    confirmSpecialCharacters = window.confirm("Should it contain special characters? (Ok = Yes | Cancel = No)");
+}
+// Colecting Edge cases for selecting no on each prompt
+    while(confirmUpperCharacters === false && confirmLowerCharacters === false && confirmNumericCharacters === false && confirmSpecialCharacters === false) {
+        window.alert("Really? You should choose at least 2!");
+        confirmUpperCharacters = window.confirm("Should it contain uppercase letters? (Ok = Yes | Cancel = No)");
+        confirmLowerCharacters = window.confirm("Should it contain lowercase letters? (Ok = Yes | Cancel = No)");
+        confirmNumericCharacters = window.confirm("Should it contain unumbers? (Ok = Yes | Cancel = No)");
+        confirmSpecialCharacters = window.confirm("Should it contain special characters? (Ok = Yes | Cancel = No)")
+    }
+//Calling the two previous functions in order to validate user choices.
+function validateUser() {
+    collectPasswordLength();
+    validatePrompts();
+}
+
+    //if the user selects a lowercase/uppercase/special/numerical character, and there is space left in the amount of character slots in the 
+//password, then add characters until the threshold is reached. Creates a password
+function gatherPassword(){
+    var result = "";
+    
+
+    while (password.passwordLength < 128) {
+        if (confirmLowerCharacters === true && password.passwordLength < confirmPassLength) {
+            var lowerCasePool = passOriginalLength[Math.floor(Math.random() * 26)]
+            resultLC = resultLC + lowerCaseLetters;
+            passOriginalLength++;
+        }
+        if (confirmUpperCharacters === true && password.passwordLength < confirmPassLength) {
+            var upperCasePool = passOriginalLength[Math.floor(Math.random() * 26)]
+            resultUC = resultUC + upperCaseLetters;
+            passOriginalLength++;
+        }
+        if (confirmNumericCharacters === true && password.passwordLength < confirmPassLength) {
+            var numberPool = passOriginalLength[Math.floor(Math.random() * 10)]
+            resultNum = resultNum + numericCharacters;
+            passOriginalLength++;
+        }
+        if (confirmSpecialCharacters === true && password.passwordLength < confirmPassLength) {
+            var specialCharacterPool = passOriginalLength[Math.floor(Math.random() * 10)]
+            resultSC = resultSC + specialCharacters;
+            passOriginalLength++;
+        }
+        return result;
+    }
+    
+    
+
+    //Reset options to start over if desired
+
+    upperCaseLetters = false;
+    lowerCaseLetters = false;
+    numbers == false;
+    specialCharacters = false;
+
+    return password;
+}
 // Write password to the #password input
 function writePassword() {
     var password = "";
@@ -35,64 +102,6 @@ function writePassword() {
     passwordText.value = password;
   
 }
-
-function collectPasswordLength() {
-    var confirmPassLength = parseInt(prompt("Please select a password length between 8 and 128 characters!"));
-    if (!confirmPassLength) {
-        alert("Please enter a value!")
-    } else if (confirmPassLength < 8 || confirmPassLength > 128) {
-        confirmPassLength = parseInt(prompt("Please choose between 8 and 128!"))
-    }
-    }
-collectPasswordLength();   
-
-// Used to determine what the user wants in their password pool.
-    confirmUpperCharacters = window.confirm("Should it contain uppercase letters? (Ok = Yes | Cancel = No)");
-    confirmLowerCharacters = window.confirm("Should it contain lowercase letters? (Ok = Yes | Cancel = No)");
-    confirmNumericCharacters = window.confirm("Should it contain numbers? (Ok = Yes | Cancel = No)");
-    confirmSpecialCharacters = window.confirm("Should it contain special characters? (Ok = Yes | Cancel = No)");
-
-// Colecting Edge cases for selecting no on each prompt
-    while(confirmUpperCharacters === false && confirmLowerCharacters === false && confirmNumericCharacters === false && confirmSpecialCharacters === false) {
-        window.alert("Really? You should choose at least 2!");
-        confirmUpperCharacters = window.confirm("Should it contain uppercase letters? (Ok = Yes | Cancel = No)");
-        confirmLowerCharacters = window.confirm("Should it contain lowercase letters? (Ok = Yes | Cancel = No)");
-        confirmNumericCharacters = window.confirm("Should it contain unumbers? (Ok = Yes | Cancel = No)");
-        confirmSpecialCharacters = window.confirm("Should it contain special characters? (Ok = Yes | Cancel = No)")
-    }
-
-    //if the user selects a lowercase/uppercase/special/numerical character, and there is space left in the amount of character slots in the 
-//password, then add characters until the threshold is reached.
-function gatherPassword(){
-    var result = "";
-    
-
-    while (passOriginalLength < confirmPassLength) {
-        if (confirmLowerCharacters === true && passOriginalLength < confirmPassLength) {
-            var lowerCaseLetters = passOriginalLength[Math.floor(Math.random() * 26)]
-            resultLC = resultLC + lowerCaseLetters;
-            passOriginalLength++;
-        }
-        if (confirmUpperCharacters === true && passOriginalLength < confirmPassLength) {
-            var upperCaseLetters = passOriginalLength[Math.floor(Math.random() * 26)]
-            resultUC = resultUC + upperCaseLetters;
-            passOriginalLength++;
-        }
-        if (confirmNumericCharacters === true && passOriginalLength < confirmPassLength) {
-            var numericCharacters = passOriginalLength[Math.floor(Math.random() * 10)]
-            resultNum = resultNum + numericCharacters;
-            passOriginalLength++;
-        }
-        if (confirmSpecialCharacters === true && passOriginalLength < confirmPassLength) {
-            var specialCharacters = passOriginalLength[Math.floor(Math.random() * 10)]
-            resultSC = resultSC + specialCharacters;
-            passOriginalLength++;
-        }
-        return result;
-    }
-    
-    }
-gatherPassword();
 
 console.log(password)
 //Randomize selected characters in a string
@@ -113,4 +122,4 @@ password.shuffle = function gatherPassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", collectPasswordLength);
--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,126 @@
-// First attempt
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-// Write password to the #password input
-
-let userPassword = {
-  // password parameters and password original state 
-  charNum: "0123456789",
-  charLower: "abcdefghijklmnopqrstuvwxyz",
-  charUpper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  charSpecial: "!@#$%^&*()",
-  passOriginalLength: 0,
-  upperValue: false,
-  lowerValue: false,
-  numberValue: false,
-  specialValue: false,
+// Password criteria stored in an obj
+let pass = {
+  passLength: 0,
+  upper: false,
+  lower: false,
+  numeric: false,
+  special: false,
+  alphabet: 'abcdefghijklmnopqrstuvwxyz',
+  numbers: '0123456789',
+  uniqueChar: ` !"#$%'()*+,-./:;<=>?@[]^_{|}~`
 }
 
-// Create alert asking user to select a password length between 8 and 128.
-// If outside parameters, alert "Try Again"
-
-//Made global for conditions outside of if statement
-
-
-//While the toContinue condition is true, continue to prompt for a value in the desired range. 
-function confirmPassLength(){
+// Makes sure user entered value for password length is a number and between 8 to 128 -- The value is saved to corresponding obj property
+function validatePasswordLength() {
+  // Value used to keep the loop running - will keep running until 'toContinue' is false
   let toContinue = true;
   while (toContinue) {
+    // The User is prompted to enter a desired length for the password. The prompt saves the value as a STRING. The 'try' block will attempt to convert the value received
+    // from the prompt to a NUMBER and save it to the 'passLength' property. If the user enters a value other than a number an error is thrown. To prevent the code from ending, 
+    // the error is dealt with by the 'catch' block which ignores it. The 'if' block checks if the user entered value meets the criteria, if it doesn't the while loop will repeat. 
     try {
-      userPassword.passOriginalLength = parseInt(window.prompt("Please enter a value between 8 and 128!"))
-      } catch (error) {
-         // If password length is inside the desired range, toContinue changes to false.
-        if(userPassword.passOriginalLength >= 8 && userPassword.passOriginalLength <=128) {
-          toContinue = false
-  }         break;
-      }
-}
-confirmPassLength();
+      pass.passLength = parseInt(window.prompt("Please enter a value for your password length. It must be a number and the value should be between 8 and 128."));
+    } catch (error) {
+      // Ignore
+    }
 
-//Collect user defined information in order to determine what characters to use for the password.
-
-
-//Validating user defined information
-function validateInfo() {
-  while (!charLower && !charNum && !charSpecial && !charUpper){
-    window.alert("Are you serious?")
-  }
-  userPassword.charUpper = window.confirm("Would you like it to contain uppercase letters?")
-  userPassword.charLower = window.confirm("Would you like it to contain lowercase letters?")
-  userPassword.charNum = window.confirm("Would you like it to contain numbers?")
-  userPassword.charSpecial = window.confirm("Would you like it to contain special characters?")
+    // If the password meets the criteria, the 'toContinue' value is updated to 'false' so the loop can exit.
+    if (pass.passLength && (pass.passLength >= 8 && pass.passLength <= 128)){
+      toContinue = false;
+    }
+  } 
 }
 
-//TODO: Call previous user input selections to determine which character inputs to use.
-var gatherPassword = [
-  function charNum() {
-    return userPassword.charNum[Math.floor(Math.random() * userPassword.charNum.length)];
+// Makes sure that the user selects yes to atleast one of the prompts and saves the value(s) to the corresponding obj property
+function validatePrompts() {
+  // Loop will continue until user selects OK on at least one prompt
+  while (!pass.upper && !pass.lower && !pass.numeric && !pass.special){
+    // Lets user know to select OK on at least one prompt
+    window.alert("Be sure to select 'OK' for at least one of the following!");
+    // A prompt is displayed for each password property and either 'true' (yes) or 'false' (no) is saved to the corresponding obj property
+    pass.upper = window.confirm("Should it contain uppercase letters? (Ok = Yes | Cancel = No)");
+    pass.lower = window.confirm("Should it contain lower case letters? (Ok = Yes | Cancel = No)");
+    pass.numeric = window.confirm("Should it contain numeric values? (Ok = Yes | Cancel = No)");
+    pass.special = window.confirm("Should it contain special characters? (Ok = Yes | Cancel = No)")
   }
-  function charNum() {
-    return userPassword.charLower[Math.floor(Math.random() * userPassword.charLower.length)];
+}
+
+// Calls previous two defined functions to validate user choices and save values to the obj properties.
+function gatherInfo() {
+  validatePasswordLength();
+  validatePrompts();
+}
+
+// Creates the password based off of the information from 'gatherInfo()'
+function generatePassword(){
+  // Updates object properties
+  gatherInfo();
+
+  // Value that stores the generated password
+  let password = '';
+  // Value used to make sure the loop continues until the password length is met
+  let iterator = 1;
+
+  // This loop runs until the amount of characters is equal to the desired password length
+  while (iterator <= pass.passLength){
+
+    // Checks if user wants upper case letters and makes sure the generated password isn't over the desired length
+    if (pass.upper & iterator <= pass.passLength){
+      // Adds a random upper case letter to the variable 'password'
+      password += pass.alphabet[Math.floor(Math.random() * pass.alphabet.length)].toUpperCase();
+      // Increases the value of 'iterator' by 1 so that it matches the amount of characters in the variable 'password'
+      iterator++;
+    }
+
+    // Checks if user wants lower case letters and makes sure the generated password isn't over the desired length
+    if (pass.lower & iterator <= pass.passLength){
+      // Adds a random lower case letter to the variable 'password'
+      password += pass.alphabet[Math.floor(Math.random() * pass.alphabet.length)];
+      // Increases the value of 'iterator' by 1 so that it matches the amount of characters in the variable 'password'
+      iterator++;
+    }
+
+    // Checks if user wants numbers and makes sure the generated password isn't over the desired length
+    if (pass.numeric & iterator <= pass.passLength){
+      // Adds a random number to the variable 'password'
+      password += pass.numbers[Math.floor(Math.random() * pass.numbers.length)];
+      // Increases the value of 'iterator' by 1 so that it matches the amount of characters in the variable 'password'
+      iterator++;
+    }
+
+    // Checks if user wants special characters and makes sure the generated password isn't over the desired length
+    if (pass.special & iterator <= pass.passLength){
+      // Adds a random upper case letter to the variable 'password'
+      password += pass.uniqueChar[Math.floor(Math.random() * pass.uniqueChar.length)];
+      // Increases the value of 'iterator' by 1 so that it matches the amount of characters in the variable 'password'
+      iterator++
+    }
+
   }
-  function charNum() {
-    return userPassword.charUpper[Math.floor(Math.random() * userPassword.charUpper.length)];
-  }
-  function charNum() {
-    return userPassword.charSpecial[Math.floor(Math.random() * userPassword.charSpecial.length)];
-  }
-];
+  
+  // Resets object properties so that another password can be generated later with different parameters if the user wants
+  pass.upper = false;
+  pass.lower = false;
+  pass.numeric = false;
+  pass.special = false;
+
+  // Returns the newly generated 'password' value so that it can be saved in a new variable
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // variable 'password' is displayed to the text area in html
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
